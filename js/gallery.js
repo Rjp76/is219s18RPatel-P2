3862253c91a339d3cfe6418b683dad568fc4f4b1
Rati Patel <31673893+Rjp76@users.noreply.github.com>
2018-04-03 10:54:21 -0400

    swapPhoto fixed, XMLHttpRequest properly functions with images.json
    Had to kind of start over, the original code was being wonky. Started using a seperate file for tests using ampps, so I copied eveything over to insure nothing was missing

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,5 @@
+/************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
+
 // requestAnim shim layer by Paul Irish
     window.requestAnimFrame = (function(){
       return  window.requestAnimationFrame       || 
@@ -19,87 +21,97 @@ var mLastFrameTime = 0;
 var mWaitTime = 5000; //time in ms
 function animate() {
     requestAnimFrame( animate );
-	var currentTime = new Date().getTime();
-	if (mLastFrameTime === 0) {
-		mLastFrameTime = currentTime;
-	}
+    var currentTime = new Date().getTime();
+    if (mLastFrameTime === 0) {
+        mLastFrameTime = currentTime;
+    }
 
-	if ((currentTime - mLastFrameTime) > mWaitTime) {
-		swapPhoto();
-		mLastFrameTime = currentTime;
-	}
+    if ((currentTime - mLastFrameTime) > mWaitTime) {
+        swapPhoto();
+        mLastFrameTime = currentTime;
+    }
 }
 
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
-
 function swapPhoto() {
- $('div#slideShow img').attr('src', mImages[mCurrentIndex].imgPath);
- $('.details #location').text('Location: '+ mImages[mCurrentIndex].imgLocation);
- $('.details #description').text("Description: " + mImages[mCurrentIndex].description);
- $('.details #date').text("Date: "+ mImages[mCurrentIndex].date);
-	//Add code here to access the #slideShow element.
-	//Access the img element and replace its source
-	//with a new image from your images array which is loaded 
-	//from the JSON string
-	//console.log('swap photo');
-    if (mCurrentIndex === mImages.length){
-        mCurrentIndex = 0;
-    } else {
-        mCurrentIndex+=1;
+    //Add code here to access the #slideShow element.
+    //Access the img element and replace its source
+    //with a new image from your images array which is loaded 
+    //from the JSON string
+     if(mCurrentIndex < mImages.length) {
+            $('.moreIndicator').click(function(){ 
+                $('.details').slideDown().toggle();
+            });
+            mCurrentIndex++;
+        } else { 
+            mCurrentIndex = 0; // resets it back to zero
+        
+        };
+    $('.photoHolder #photo').attr("src", mImages[mCurrentIndex].image);
+    $('.location').text('Location: ' + mImages[mCurrentIndex].location);
+    $('.description').text('Description: ' + mImages[mCurrentIndex].description);
+    $('.date').text('Date: ' + mImages[mCurrentIndex].date);
+    
+    
+       
+        
+    console.log('swap photo');
+    console.log(mImages[mCurrentIndex].description);
+    
+};
+var mCurrentIndex = 0;
+function makegalleryImageOnloadCallback(galleryImage) {
+    return function(e) {
+        galleryImage.img = e.target;
+        mImages.push(galleryImage);
     }
 }
-function galleryImage(imgPath, imgLocation, description, date) {
-    this.imgPath = imgPath;
-    this.imgLocation = imgLocation;
-    this.description = description;
-    this.date = date;
-   }
-// Counter for the mImages array
-    var mCurrentIndex = 0;
-// XMLHttpRequest variable
-// Array holding GalleryImage objects (see below).
-    var mImages = [];
-// Holds the retrived JSON information
-    var mJson;
-// URL for the JSON to load by default
-// Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-    var mURL = "images.json";
-    var mRequest = new XMLHttpRequest();
-    mRequest.onReadyStateChange = function() {
-            if (mRequest.readyState === 4 && mRequest.status === 200) {
-                try {
-                    mJson = JSON.parse(mRequest.responseText);
-                    for (var i = 0; i < mJson.images.length ; i++){
-                    mImages.push(new galleryImage(mJson.images[i].imgPath,mJson.images[i].imgLocation,mJson.images[i].description,mJson.images[i].date));
-                }
-                } catch(err) {
-                    console.log(err.message)
-                }
+
+$(document).ready( function() {
+    // This initially hides the photos' metadata information
+  //  $('.details').eq(0).hide();
+  $('.details').eq(0).show();
+    
+});
+
+window.addEventListener('load', function() {
+    
+    console.log('window loaded');
+
+}, false);
+
+
+function galleryImage(path, place, descrp, d) {
+    this.image = path;
+    this.location = place;
+    this.description = descrp;
+    this.date = d;
+}
+
+var mImages = [];
+
+for (var i = 0; i < mImages.length; i++) {
+console.log(mImages[i]); 
+}
+var mURL = "images.json";
+var mRequest = new XMLHttpRequest();
+mRequest.onreadystatechange = function() {
+    // Do something interesting if file is opened successfully
+    if (mRequest.readyState == 4 && mRequest.status == 200) {
+        try {
+            mJson = JSON.parse(mRequest.responseText);
+            for(var i = 0; i<mJson.images.length; i++) { 
+                var imgloc = mJson.images[i].imgPath;
+                var loca = mJson.images[i].imgLocation;
+                var desc = mJson.images[i].description;
+                var d = mJson.images[i].date;
+                mImages.push(new galleryImage(imgloc, loca, desc, d));
             }
-        };
-    mRequest.open("GET", mURL , true);
-    mRequest.send();
-
-
-//You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
-//@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
-    function makeGalleryImageOnloadCallback(galleryImage) {
-        return function (e) {
-            galleryImage.img = e.target;
-            mImages.push(galleryImage);
+            console.log(mJson);
+        } catch(err) {
+            console.log(err.message);
         }
     }
-
-    $(document).ready(function () {
-        $('.details').show();
-        animate();
-
-    });
-
-    window.addEventListener('load', function () {
-
-        console.log('window loaded');
-
-    },true);
-
-
+};
+mRequest.open("GET",mURL, true);
+mRequest.send();
